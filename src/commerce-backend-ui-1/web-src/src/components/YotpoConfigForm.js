16cc123b7a479c6ff209b224a5ed2b94a2aa70bb
Doug Hatcher <doug.hatcher@blueacornici.com>
2025-06-07 11:40:30 -0400

    adds support links

--- src/commerce-backend-ui-1/web-src/src/components/YotpoConfigForm.js
+++ src/commerce-backend-ui-1/web-src/src/components/YotpoConfigForm.js
@@ -8,9 +8,14 @@ import {
   Picker,
   Item,
   Heading,
+  Grid,
+  Link,
+  Text
 } from "@adobe/react-spectrum";
 
 const DEBUG = false; // Set to true for detailed error messages
+
+
 
 export default function YotpoConfigForm({ actionUrl }) {
   const [appKey, setAppKey] = useState("");
@@ -67,6 +72,14 @@ export default function YotpoConfigForm({ actionUrl }) {
     }
   }
 
+const links = [
+    { label: 'Blue Acorn iCi', url: 'https://blueacornici.shop/' },
+    { label: 'Create an Issue', url: 'https://github.com/BlueAcornInc/aio-commerce-yotpo/issues/new' },
+    { label: 'Issue Tracker', url: 'https://github.com/BlueAcornInc/aio-commerce-yotpo/issues' },
+    { label: 'Contact Us', url: 'mailto:apps@blueacornici.com' },
+    { label: 'Documentation', url: 'https://apps.blueacornici.shop/' },
+]
+
 return (
     <View padding="size-250">
         <Content marginBottom="size-200">{statusMsg}</Content>
@@ -117,6 +130,26 @@ return (
                 </Content>
             )}
 
+            <br /><br />
+            <Heading level={3}>Support</Heading>
+            <Grid
+                columns={['1fr 1fr']}
+                gap="size-200"
+                width="size-3600"
+            >
+                {links.map(link => (
+                    <View key={link.url} borderWidth="thin" borderColor="dark" padding="size-200" borderRadius="medium">
+                        <a
+                            href={link.url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            style={{ textDecoration: 'none', color: 'inherit' }}
+                        >
+                            <Text>{link.label}</Text>
+                        </a>
+                    </View>
+                ))}
+            </Grid>
         </Form>
     </View>
 );
